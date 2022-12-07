7bf9760e982248d9ca1d7b1fdfe2a8129ea40135
santiagoss01 <santiagozehcnas@gmail.com>
2022-12-07 13:21:42 +0000

    My second commit with styles

--- src/js/component/TodoListcomplete.jsx
+++ src/js/component/TodoListcomplete.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 const NewList = () => {
     const [myinput, setMyinput] = useState("");
 
-    const [myList, setMylist] = useState(["Reading", "Cooking","dancing-alone","Playing with my cats"]);
+    const [myList, setMylist] = useState(["Reading", "Cooking", "dancing-alone", "Playing with my cats"]);
 
     const submitHandler = (e) => {
         e.preventDefault();
@@ -11,33 +11,42 @@ const NewList = () => {
         setMylist([myinput, ...myList]);
 
     };
- 
+
     const controlledInput = (e) => {
         setMyinput(e.target.value);
     };
 
     const removeFromlist = (index) => {
-        setMylist(myList.filter((element,id)=>{
-          return index !== id;
+        setMylist(myList.filter((element, id) => {
+            return index !== id;
         }))
-      };
-    
+    };
+    const [applyStyle, setApplystyle] = useState("");
+
+    // const taskCompleted = (e) => {
+    //     myList.map((element, id) => {
+    //         if (e.target.id === id) 
+    //             setApplystyle("");
+    //        else if (e.target.id !== id) 
+    //         setApplystyle(" red");})}
+
+
 
     return (
         <div>
-            <h1>Task Manager</h1>
+            <h1>Task Manager</h1><h1 id="task-two">Task Manager</h1>
             <form className="form-group-toDo" onSubmit={submitHandler}>
-                <input type="text" onChange={controlledInput}  value={myinput} className="form" placeholder="Add task here" />
+                <input type="text" onChange={controlledInput} value={myinput} className="form text-center" placeholder="ADD YOUR NEW TASK" />
             </form>
 
-            <div>
+            <div className=" container d-flex justify-content-center">
                 <ul className="list-group">
                     {myList.map((listElement, index) => {
-                        return <li key={index} className="list-group-item d-flex justify-content-between hidden-icon">
+                        return <li key={index} onClick={(e) => { taskCompleted(e) }} className={"list-group-item d-flex  justify-content-between hidden-icon myStyledlist"}>
                             {listElement}
                             <span>
-                                <a key={index} onClick={(e) => {removeFromlist(index)}}>
-                                    <i className="fas fa-trash"></i>
+                                <a key={index} onMouseOver={(e) => { setApplystyle(" red") }} onMouseOut={(e) => { setApplystyle("") }} onClick={(e) => { removeFromlist(index) }}>
+                                    <i className={"fas fa-trash selected " + applyStyle}></i>
                                 </a>
                             </span>
                         </li>
@@ -45,6 +54,7 @@ const NewList = () => {
                     }
 
                 </ul>
+                {myList.length ? "" : <span className="warnign"> Add a new task!</span>}
             </div>
         </div>
 
