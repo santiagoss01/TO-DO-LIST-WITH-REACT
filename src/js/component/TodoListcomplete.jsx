321c7ecea38cd6a80338bab5e6df093f83c25e1f
santiagoss01 <santiagozehcnas@gmail.com>
2022-12-13 17:48:32 +0000

    Functionalities and object approach fixed

--- src/js/component/TodoListcomplete.jsx
+++ src/js/component/TodoListcomplete.jsx
@@ -1,81 +1,136 @@
 import React, { useState } from "react";
 
 const NewList = () => {
-    const [myinput, setMyinput] = useState("");
+ 
+ let [myinput, setMyinput] = useState("");
 
-    const [myList, setMylist] = useState(["Reading", "Cooking", "dancing alone", "Cat food"]);
 
-    const submitHandler = (e) => {
-        e.preventDefault();
-        setMyinput("");
-        setMylist([myinput, ...myList]);
-
-    };
-
-    const controlledInput = (e) => {
-        setMyinput(e.target.value);
-    };
-
-    const removeFromlist = (index) => {
-        setMylist(myList.filter((element, id) => {
-            return index !== id;
-        }))
-    };
-    const [applyStyle, setApplystyle] = useState("");
-     
-    const[completedTodo, setCompletedtodo] = useState([]);
-
-    const taskCompleted = (index) => {
-        setCompletedtodo([...completedTodo, index]);
-    }
-
-    const[imporTanttodo, setimporTanttodo] = useState([]);
-
-    const importantTask = (index) => {
-        setimporTanttodo([...imporTanttodo, index]);
-    }
+  const myObject = {
+    label: myinput,
+    completada: false,
+    importante:false
+  }
 
 
 
+  const [myList, setMylist] = useState([
+    {
+      label: "Watching chainsaw man",
+      completada: false,
+      importante: false,
+    },
+  ]);
 
-    return (
-        <div>
-            <h1>Task Manager</h1><h1 id="task-two">Task Manager</h1>
-            <form className="form-group-toDo" onSubmit={submitHandler}>
-                <input type="text" onChange={controlledInput} value={myinput} className="form text-center" placeholder="ADD YOUR NEW TASK" />
-            </form>
+  const controlledInput = (e) => {
+    setMyinput(e.target.value);
+  };
+  const submitHandler = (e) => {
+    e.preventDefault();
 
-            <div className=" container d-flex justify-content-center">
-                <ul className="list-group">
-                    {myList.map((listElement, index) => {
-                        return <li key={index} className= {`list-group-item d-flex  justify-content-between hidden-icon myStyledlist ${completedTodo.includes(index)? " red":""} ${imporTanttodo.includes(index)? " important":""}`} >
-                            {listElement}
-                            <span className="d-flex justify-content-around">
-                                
-                                <a id="trash" key={index}  onClick={(e) => { removeFromlist(index) }}>
-                                    <i className={"fas fa-trash selected "}></i>
-                                </a>
-                                <a id="select" key={index} onClick={(e) =>{taskCompleted(index)}}>
-                                    <i className={"fas fa-check selected "}></i>
-                                </a>
-                            
-                                <a id="check" key={index} onClick={(e) =>{importantTask(index)}}>
-                                    <i className={"fas fa-exclamation selected "}></i>
-                                </a>
-                                
-                                
-                            </span>
-                        </li>
-                    })
-                    }
+    
 
-                </ul>
-                {myList.length ? "" : <span id="warning"> Add a new task!</span>}
-            </div>
-        </div>
+    setMylist([
+  
+      ...myList, myObject
+    ]);
 
+    setMyinput("");
+  };
+
+  const removeFromlist = (index) => {
+    setMylist(
+      myList.filter((element, id) => {
+        return index !== id;
+      })
     );
+  };
 
+
+
+  const actualizarTarea = (indice,propiedad,valor) => {
+    
+   
+    const nuevoObject = myList[indice];
+
+    nuevoObject[propiedad] = valor;
+
+    setMylist(myList.map((value, index)=>{
+      if(index===indice){
+        return nuevoObject;
+      }
+      else return value;
+    }))
+   
+    console.log(indice,propiedad,valor);
+  };
+    
+    
+
+ 
+  
+
+  return (
+    <div>
+      <h1>Task Manager</h1>
+      <h1 id="task-two">Task Manager</h1>
+      <form className="form-group-toDo" onSubmit={submitHandler}>
+        <input
+          type="text"
+          onChange={controlledInput}
+          value={myinput}
+          className="form text-center"
+          placeholder="ADD YOUR NEW TASK"
+        />
+      </form>
+
+      <div className=" container d-flex justify-content-center">
+        <ul className="list-group">
+          {myList.map((listElement, index) => {
+            return (
+              <li
+                key={index}
+                className={`list-group-item d-flex  justify-content-between hidden-icon myStyledlist ${
+                  listElement.completada == true ? " red" : ""
+                } ${listElement.importante == true ? " important" : ""}`}
+              >
+                {listElement.label}
+
+                <span className="d-flex justify-content-around">
+                  <a
+                    id="trash"
+                    onClick={(e) => {
+                      removeFromlist(index);
+                    }}
+                  >
+                    <i className={"fas fa-trash selected "}></i>
+                  </a>
+
+                  <a
+                    id="select"
+                    onClick={(e) => {
+                      actualizarTarea(index,"completada", true);
+                    }}
+                  >
+                    <i className={"fas fa-check selected "}></i>
+                  </a>
+
+                  <a
+                    id="check"
+                    onClick={(e) => {
+                      actualizarTarea(index, "importante", true);
+                    }}
+                  >
+                    <i className={"fas fa-exclamation selected "}></i>
+                  </a>
+                </span>
+              </li>
+            );
+          })}
+        </ul>
+        {myList.length ? "" : <span id="warning"> Add a new task!</span>}
+      </div>
+    </div>
+  );
 };
 
 export default NewList;
